08ccede35dbf367176e056b67316075e74ce6177
Paulixor <39218124+Paulixor@users.noreply.github.com>
2018-08-11 21:54:25 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -21,27 +21,27 @@ bot.on ("message", async message => {
     let args = messageArray.slice(1);
 
     bot.on("guildMemberAdd", member => {
-    const bvn = member.guild.channels.find("name", "réception")
-    if(!bvn)return;
+    const bvn = member.guild.channels.find(`name`, "réception")
+    if(!bvn) return;
     var embed = new Discord.RichEmbed()
             .setAuthor("L.G.G.#1073", bot.user.avatarURL)
             .setTitle("Arrivée d'un nouveau membre !")
-            .addField(`Bienvenue à ${member.user.tag} !`,"Fais )help pour utiliser les commndes de Loys !", true)
+            .addField(`Bienvenue à ${member.user.tag} !`,"Fais )help pour utiliser les commandes de Loys !", true)
             .addField(`Tu es le ${member.guild.memberCount}ème membre`, `N'oublie pas de lire le <#372382732677808129> et passe du bon temps sur le serveur !`)
             .setColor("0x04B404")
-            .setFooter("Bienvnenue dans le LoysCorp Global Group (L.G.G.) !")
+            .setFooter("Bienvnenue dans le LoysCorp Global Group (L.G.G.) !");
         message.channel.send(embed)
 
 })
 bot.on("guildMemberRemove", member => {
-    const bvn = member.guild.channels.find("name", "réception")
-    if(!bvn)return;
+    const bvn = member.guild.channels.find(`name`, "réception")
+    if(!bvn) return;
     var embed = new Discord.RichEmbed()
             .setAuthor("L.G.G.#1073", bot.user.avatarURL)
             .setTitle("Départ d'un membre")
             .addField(`Départ de ${member.user.tag} !`,"Nous sommes désolés que tu ne te sois pas plu ici. Bonne continuation !", true)
             .setColor("0f50505")
-            .setFooter("Au revoir !")
+            .setFooter("Au revoir !");
         message.channel.send(embed)
 
 })
