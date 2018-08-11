b60fc3600dbd71b5d62e6d7f5ba3279f0b234137
Paulixor <39218124+Paulixor@users.noreply.github.com>
2018-08-11 21:34:16 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,12 +10,29 @@ bot.on("ready", () => {
                                                          /// Bienvenue
 
 bot.on("guildMemberAdd", member => {
-if(!member.guild.id === "371641779952287744") return
-    member.guild.channels.find("name", "réception").send(`:check: Bienvenue à toi, ${member} :tada: \nN'oublie pas de lire le <#372382732677808129> et passe du bon temps sur le serveur !`)
+    const bvn = member.guild.channels.find(m => m.name === "réception")
+    if(!bvn)return;
+    var embed = new Discord.RichEmbed()
+            .setAuthor("L.G.G.#1073", bot.user.avatarURL)
+            .setTitle("Arrivée d'un nouveau membre !")
+            .addField(`Bienvenue à ${member.user.tag} !`,"Fais )help pour utiliser les commndes de Loys !", true)
+            .addField(`Tu es le ${member.guild.memberCount}ème membre`, `N'oublie pas de lire le <#372382732677808129> et passe du bon temps sur le serveur !`)
+            .setColor("0x04B404")
+            .setFooter("Bienvnenue dans le LoysCorp Global Group (L.G.G.) !")
+        message.channel.send(embed)
+
 })
 bot.on("guildMemberRemove", member => {
-if(!member.guild.id === "371641779952287744") return
-    member.guild.channels.find("name", "réception").send(`:x: ${member} vient de quitter le serveur :pensive:. Au revoir !`)
+    const bvn = member.guild.channels.find(m => m.name === "réception")
+    if(!bvn)return;
+    var embed = new Discord.RichEmbed()
+            .setAuthor("L.G.G.#1073", bot.user.avatarURL)
+            .setTitle("Départ d'un membre")
+            .addField(`Départ de ${member.user.tag} !`,"Nous sommes désolés que tu ne te sois pas plu ici. Bonne continuation !", true)
+            .setColor("0f50505")
+            .setFooter("Au revoir !")
+        message.channel.send(embed)
+
 })
 bot.on(`guildMemberAdd`, member => {
 if(!member.guild.id === "371641779952287744") return
